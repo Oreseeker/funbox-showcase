1fb589f0cfe1450e17e30673b854dfe19c5ae337
Oreseeker <veniaminamvrosov@g,ail.com>
2020-09-01 11:35:53 +0300

    Fixed weight circle, done with card effect on select/hover

--- js/scripts.js
+++ js/scripts.js
@@ -30,7 +30,11 @@ const card = Vue.component('card', {
 		}
 	},
 	template: `<div class="card">
-				  <div :class="{'card-main': true, 'no-select': true, 'card-main-selected': cardSelected }" v-on:click="cardSelected=!cardSelected; onceLeft=false" 
+				  <div class="card-main no-select" 
+				  :class="{'card-main-selected': this.cardSelected, 'card-main-hover': this.cardHover, 
+				  'card-main-hover-selected': this.cardSelected && this.cardHover,  
+				  'card-main-default': !this.cardSelected && !this.cardHover}" 
+				  v-on:click="cardSelected=!cardSelected; onceLeft=false"
 				  v-on:mouseover="cardHover=true" v-on:mouseleave="cardHover=false; onceLeft=true" :disabled="!options.inStock">
 				      <p :class="{'common-text': true, 'p-highlighted': cardHover && cardSelected && onceLeft}">{{headerPhrase()}}</p>
 				      <h1>Нямушка</h1>
@@ -38,13 +42,21 @@ const card = Vue.component('card', {
 				      <p><b>{{options.portions}}</b> {{portionPhrase()}}</p>
 				      <p v-if="options.numberOfMice">
 				      <b v-if="options.numberOfMice != 1">{{options.numberOfMice}}</b> {{micePhrase()}} в подарок</p>
-				      <div :class="{'weight': true, 'weight-value-selected': cardSelected}">
-					      <span class="weight-value">{{options.weight}}</span>
-					      <br>
-					      <span class="weight-units">кг</span>
+				      <div :class="{'weight': true, 'weight-selected': cardSelected, 
+				      'weight-default': !cardSelected && !cardHover, 'weight-default-hover': cardHover, 
+				      'weight-selected-hover': cardHover && cardSelected && onceLeft}">
+					      <div class="weight-value">{{options.weight}}</div>
+					      <div class="weight-units">кг</div>
 				      </div>
 				  </div>
-				  <p :class="{'card-comment': true, 'y-highlighted': !options.inStock}" v-html="statusPhrase()">
+				  <p :class="cardCommentClasses" v-if="!options.inStock">
+				  	Печалька, с {{options.withTaste}} закончился
+				  </p>
+				  <p :class="cardCommentClasses" v-else-if="cardSelected">
+				  	{{options.description}}
+				  </p>
+				  <p :class="cardCommentClasses" v-else="!options.inStock">
+				  	Чего сидишь? Порадуй котэ, <a class="dotted no-select" v-on:click="cardSelected=true">купи.</a>
 				  </p>
 			   </div>
 			  `,
@@ -53,6 +65,7 @@ const card = Vue.component('card', {
 			cardHover: false,
 			cardSelected: false,
 			onceLeft: false,
+			cardCommentClasses: {'card-comment': true, 'y-highlighted': !this.options.inStock},
 		}
 	},
 	methods: {
@@ -90,26 +103,19 @@ const card = Vue.component('card', {
 				return "мышей";
 			}
 		},
-		statusPhrase() {
-			if (!this.options.inStock) {
-				return `Печалька, с ${this.options.withTaste} закончился`;
-			}
-			if (this.cardSelected) {
-				return this.options.description;
-			}
-			else {
-				return `Чего сидишь? Порадуй котэ, <a class="dotted no-select" v-on:click="cardSelected=true">купи.</a>`;
-			}
-		}
 	},
 	mounted() {
 		/* --- preloading images (for better user @click experience) --- */
 		let defaultImage = new Image();
+		let hoverImage = new Image();
 		let selectedImage = new Image();
+		let selectedHoverImage = new Image();
 		defaultImage.src = 'images/cat_default.png';
 		selectedImage.src = 'images/cat_selected.png';
+		selectedHoverImage.src = 'images/cat_selected_hover.png';
+		hoverImage.src = 'images/cat_default_hover.png';
 		/* --- end preloading images ---*/
-	}
+	},
 });
 
 const app = new Vue({
@@ -119,12 +125,12 @@ const app = new Vue({
 			portions: 1,
 			withTaste: "фуа-гра",
 			numberOfMice: 1,
-			weight: '0.5',
+			weight: '0,5',
 			inStock: true,
 			description: "Печень утки разварная с артишоками.",
 		},
 		fish: {
-			portions: 100,
+			portions: 10,
 			withTaste: "рыбой",
 			numberOfMice: 2,
 			weight: '2',
@@ -132,7 +138,7 @@ const app = new Vue({
 			description: "Головы щучьи с чесноком да свежайшая сёмгушка.",
 		},
 		chicken: {
-			portions: 1,
+			portions: 100,
 			withTaste: "курой",
 			numberOfMice: 5,
 			weight: '10',
